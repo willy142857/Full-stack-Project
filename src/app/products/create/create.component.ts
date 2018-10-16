cba137bc0a4de0cc7954bab90212a2f464f8a670
WillyYang <willy142857@gmail.com>
2018-10-17 00:20:20 +0800

    增加新增商品頁面

--- src/app/products/create/create.component.ts
+++ src/app/products/create/create.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create.component.css']
 })
 export class CreateComponent implements OnInit {
+  product = new Product();
+  name = this.product.name;
+  price = this.product.price;
+  description = this.product.description;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  printData() {
+    console.log(this.name + '\n' + this.price + '\n' + this.description);
   }
-
+}
+export class Product {
+  name = '';
+  price: number;
+  description = '';
 }
