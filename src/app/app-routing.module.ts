1d0824423ec08c0c99496c75a77cf12560d383ff
WillyYang <willy142857@gmail.com>
2018-10-17 18:31:09 +0800

    add productService and update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,24 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { LayoutComponent } from './layout/layout.component';
 import { CartComponent } from './cart/cart.component';
+import { CreateComponent } from './products/create/create.component';
 
 const routes: Routes = [
   {
-    path: 'cart',
-    component: CartComponent
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'create',
+        component: CreateComponent
+      }
+    ]
+  },
+  {
+    path: 'login',
+    component: LoginComponent
   }
 ];
 
