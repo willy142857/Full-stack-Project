c2890b1f99d875269fcef3d972e10973ddd7c267
B10532036 <B10532036@mail.ntust.edu.tw>
2018-10-20 19:58:28 +0800

    修正register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MemberService } from '../member/member.service';
 
 @Component({
   selector: 'app-register',
@@ -6,11 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  constructor() {}
-
+  constructor(private memberService: MemberService) {}
   model: any = {};
   ngOnInit() {}
   onSubmit(form) {
     console.log(this.model);
+    this.memberService.member.push(this.model);
+    console.log(this.memberService.member);
   }
 }
