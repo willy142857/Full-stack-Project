a742cee9d59411820c183668d9056bb3854d0076
WillyYang <willy142857@gmail.com>
2018-10-21 00:27:19 +0800

    增加多國語系
    需安裝套件
    npm install @ngx-translate/core --save
    npm install @ngx-translate/http-loader --save
    相關網站:
    https://bit.ly/2Sa18S6
    https://github.com/ngx-translate/core

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -17,6 +18,11 @@ import { IndexComponent } from './products/index/index.component';
 import { CartComponent } from './cart/cart.component';
 import { CommodityComponent } from './commodity/commodity.component';
 
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 @NgModule({
   declarations: [
     AppComponent,
@@ -32,7 +38,19 @@ import { CommodityComponent } from './commodity/commodity.component';
     CartComponent,
     CommodityComponent
   ],
-  imports: [BrowserModule, HttpClientModule, FormsModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: createTranslateLoader,
+        deps: [HttpClient]
+      }
+    })
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
