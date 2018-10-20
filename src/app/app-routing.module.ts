203da7e775f609d59713d2c0fc6b4b48e1ffcddb
WillyYang <willy142857@gmail.com>
2018-10-20 16:36:18 +0800

    update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { LoginComponent } from './login/login.component';
 import { LayoutComponent } from './layout/layout.component';
 import { CartComponent } from './cart/cart.component';
 import { CreateComponent } from './products/create/create.component';
+import { EditComponent } from './products/edit/edit.component';
+import { IndexComponent } from './products/index/index.component';
 
 const routes: Routes = [
   {
@@ -11,14 +13,22 @@ const routes: Routes = [
     component: LayoutComponent,
     children: [
       {
+        path: 'index',
+        component: IndexComponent
+      },
+      {
         path: 'create',
         component: CreateComponent
+      },
+      {
+        path: 'edit:/id',
+        component: EditComponent
+      },
+      {
+        path: 'login',
+        component: LoginComponent
       }
     ]
-  },
-  {
-    path: 'login',
-    component: LoginComponent
   }
 ];
 
