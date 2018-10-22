61292467b3b66c63dead6e8149ac10adb662c642
B10532021 <b10532021@gapps.ntust.edu.tw>
2018-10-23 00:53:27 +0800

    commodity update版面配置

--- src/app/commodity/commodities.service.ts
+++ src/app/commodity/commodities.service.ts
@@ -13,12 +13,33 @@ export class CommoditiesService {
       brief_intro: '這個商品真的很好.............................................................',
       get: 50000,
       people: 50,
-      createdAt: '2018-09-11T19:49:35.917Z',
-      updatedAt: '2018-11-11T19:49:35.917Z',
+      startAt: '2018-09-11T19:49:35.917Z',
+      endAt: '2018-11-11T19:49:35.917Z',
       email: '1234@gmail.com',
       description: 'asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf',
       feedback: '500',
       feedback_description: '1.for free \n 2.good \n 3.awesome\n',
       connect: 'http://www.google.com',
     };
+
+  messages = [
+    {
+      name: 'John',
+      sum_review: 'good product',
+      review: 'I use this product as a ......, and it is very practical',
+      date: '2018-10-13T19:49:35.917Z'
+    },
+    {
+      name: 'Max',
+      sum_review: 'good product',
+      review: 'I use this product as a ......, and it is very practical',
+      date: '2018-09-20T19:49:35.917Z'
+    },
+    {
+      name: 'Howard',
+      sum_review: 'good product',
+      review: 'I use this product as a ......, and it is very practical',
+      date: '2018-10-17T19:49:35.917Z'
+    }
+  ];
 }
--- src/app/commodity/commodity.component.ts
+++ src/app/commodity/commodity.component.ts
@@ -13,9 +13,27 @@ export class CommodityComponent implements OnInit {
     console.log(this.today);
     return this.commoditiesService.commodity;
   }
-  // deadline: number = Date(this.list.updatedAt);
-  constructor(private commoditiesService: CommoditiesService) { }
-  ngOnInit() {
-  }
 
+  get messages() {
+    console.log(this.today);
+    return this.commoditiesService.messages;
+  }
+  diff: number;
+  diffDays: number;
+  date() {
+    if (new Date().getTime() < new Date(this.list.startAt).getTime()) {
+      return '未开始';
+    } else if (new Date().getTime() > new Date(this.list.endAt).getTime()) {
+      return '已结束';
+    } else if (
+      new Date(this.list.startAt).getTime() < new Date().getTime() &&
+      new Date().getTime() < new Date(this.list.endAt).getTime()) {
+      this.diff = Math.abs(new Date(this.list.endAt).getTime() - new Date().getTime());
+      this.diffDays = Math.ceil(this.diff / (1000 * 3600 * 24));
+      return this.diffDays;
+    }
+  }
+  // deadline: number = Date(this.list.updatedAt);
+  constructor(private commoditiesService: CommoditiesService) {}
+  ngOnInit() {}
 }
