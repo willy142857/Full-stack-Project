93e59c73b1ab26877cb2fa67c6a936b5cf9dcd09
WillyYang <willy142857@gmail.com>
2018-10-24 03:31:23 +0800

    新增測試資料

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -4,9 +4,25 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ProductService {
+  test = {
+    founder: '約瑞柯文哲',
+    email: 'google@.gmail.com',
+    startAt: '2018-10-24',
+    endAt: '2018-11-24',
+    name: '票投姚文智，電狼3-4',
+    targetPrice: '7777777',
+    brief: 'QQPRQQPR',
+    description: '980980980980980980',
+    feedbackPirce: '87',
+    feedbackDescription: 'qwertyuiop',
+    feedbackAt: '2187-01-01',
+    relativeWeb: 'google.com'
+  };
   constructor() {}
   set() {}
-  get() {}
+  get() {
+    return this.test;
+  }
 }
 
 export class Product {
