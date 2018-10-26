cd17b5e68aa59b4dabc0df9038018c382b1349fe
WillyYang <willy142857@gmail.com>
2018-10-26 22:16:03 +0800

    移除layout 修改商品顯示

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { CommodityComponent } from './commodity/commodity.component';
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { LoginComponent } from './member/login/login.component';
-import { LayoutComponent } from './layout/layout.component';
 import { CartComponent } from './cart/cart.component';
 import { CheckoutComponent } from './checkout/checkout.component';
 import { RegisterComponent } from './member/register/register.component';
@@ -13,46 +12,36 @@ import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   {
-    path: '',
-    component: LayoutComponent,
-    children: [
-      {
-        path: '',
-        loadChildren: './product/product.module#ProductModule'
-      },
-      {
-        path: 'login',
-        component: LoginComponent
-      },
-      {
-        path: 'register',
-        component: RegisterComponent
-      },
-      {
-        path: 'cart',
-        component: CartComponent
-      },
-      {
-        path: 'checkout',
-        component: CheckoutComponent
-      },
-      {
-        path: 'commodity',
-        component: CommodityComponent
-      },
-      {
-        path: 'resetPassword',
-        component: ResetpasswordComponent
-      },
-      {
-        path: 'retrievePassword',
-        component: RetrievepasswordComponent
-      },
-      {
-        path: 'profile',
-        component: ProfileComponent
-      }
-    ]
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'cart',
+    component: CartComponent
+  },
+  {
+    path: 'checkout',
+    component: CheckoutComponent
+  },
+  {
+    path: 'commodity',
+    component: CommodityComponent
+  },
+  {
+    path: 'resetPassword',
+    component: ResetpasswordComponent
+  },
+  {
+    path: 'retrievePassword',
+    component: RetrievepasswordComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
   }
 ];
 
