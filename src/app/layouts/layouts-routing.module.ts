40c3a8c11f103f49be0a54f73c44646a0b9930f8
ChinYa <smallrespect33@gmail.com>
2018-11-01 16:38:06 +0800

    新增footer常見問答的頁面

--- src/app/layouts/layouts-routing.module.ts
+++ src/app/layouts/layouts-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { QAComponent } from './qa/qa.component';
+
+const routes: Routes = [
+  {
+    path: 'qa',
+    component: QAComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
