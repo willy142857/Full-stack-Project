9ddcf729a017b37bbae9321e1e3544a586ec1b15
WillyYang <willy142857@gmail.com>
2018-11-19 03:14:05 +0800

    調整提案頁面

--- src/app/project/create/create.component.ts
+++ src/app/project/create/create.component.ts
@@ -1,22 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { ProjectService } from '../project.service';
 import { Router } from '@angular/router';
+import { Project, Category } from '../project';
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.css']
 })
 export class CreateComponent implements OnInit {
-  model: any = {};
-  category = this.projectService.category;
+  project: any = {};
+  categories: Category[];
   nowDate = new Date();
   constructor(private projectService: ProjectService, private router: Router) {}
+
   ngOnInit() {
     scroll(0, 0);
+    this.projectService.getCategories().subscribe((categories: Category[]) => {
+      this.categories = categories;
+    });
   }
-  onSubmit(form) {
-    this.projectService.set();
+
+  onSubmit() {
+    //this.projectService.createProject(this.project);
+    console.log(this.project);
     this.router.navigateByUrl('/');
-    console.log(form.value);
   }
 }
