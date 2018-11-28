093b0d5e856c5be2ec719222ee5cb53be9ea7364
ChinYa <smallrespect33@gmail.com>
2018-11-28 17:09:28 +0800

    footer翻譯

--- src/app/layouts/nav/nav.component.ts
+++ src/app/layouts/nav/nav.component.ts
@@ -6,16 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-  art = '藝術';
-  story = '故事牆';
-  shareStory = '分享你的故事';
-  explore = '探索';
-  total = '所有計畫';
-  final = '最後衝刺';
-  new = '最新啟動';
-  proposal = '提案';
-  shorttime = '一次性集資';
-  longtime = '訂閱式專案';
+
   constructor() {}
   ngOnInit() {
     // this.layoutsService.currentPage = 1;
