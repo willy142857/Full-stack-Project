851161c698667b08347e72a7a1fe5574ebaa728b
ChinYa <smallrespect33@gmail.com>
2018-11-29 02:50:26 +0800

    小修

--- src/app/layouts/nav/nav.component.ts
+++ src/app/layouts/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common' ;
 
 @Component({
   selector: 'app-nav',
@@ -7,8 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() {}
+  constructor(private location: Location) {}
+  pathUrl = this.location.path(); // 獲取當前url的子路徑(也就是當前url#後面的内容,不包括参數)
   ngOnInit() {
-    // this.layoutsService.currentPage = 1;
+    scroll(0, 0);
+    console.log(this.pathUrl);
   }
+
+  getPath() {
+    /*if (this.pathUrl !== '/create' && this.pathUrl !== '/project') {
+
+    }*/
+  }
+  /*getCreate() {
+    console.log(this.pathUrl);
+    if (this.pathUrl === '/create') {
+      '.current-page-item = true';
+    }
+  }
+  getProject() {
+    console.log(this.pathUrl);
+    if (this.pathUrl === '/project') {
+
+    }
+  }*/
 }
