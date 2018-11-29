911814fdea415419c5418f4a102f1e3ccd681e66
ChinYa <smallrespect33@gmail.com>
2018-11-29 20:59:21 +0800

    找當前頁面顯示css

--- src/app/layouts/nav/nav.component.ts
+++ src/app/layouts/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common' ;
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -8,28 +9,27 @@ import { Location } from '@angular/common' ;
 })
 export class NavComponent implements OnInit {
 
-  constructor(private location: Location) {}
-  pathUrl = this.location.path(); // 獲取當前url的子路徑(也就是當前url#後面的内容,不包括参數)
+  constructor(private location: Location,
+    private route: ActivatedRoute
+    ) {}
+  pathUrl;
+  routeUrl;
+  current = {
+    'current-page-item': false,
+  };
   ngOnInit() {
     scroll(0, 0);
-    console.log(this.pathUrl);
+    this.check();
   }
-
-  getPath() {
-    /*if (this.pathUrl !== '/create' && this.pathUrl !== '/project') {
-
-    }*/
-  }
-  /*getCreate() {
-    console.log(this.pathUrl);
-    if (this.pathUrl === '/create') {
-      '.current-page-item = true';
+  check() {
+    this.pathUrl = this.location.path(); // 獲取當前url的子路徑(也就是當前url#後面的内容,不包括参數)
+    this.routeUrl = this.route.url;
+    if (this.pathUrl.indexOf('projects') !== -1) {
+      this.pathUrl = '/projects';
+    } else if (this.pathUrl.indexOf('create') !== -1) {
+      this.pathUrl = '/create';
     }
-  }
-  getProject() {
+    console.log(this.routeUrl);
     console.log(this.pathUrl);
-    if (this.pathUrl === '/project') {
-
-    }
-  }*/
+  }
 }
