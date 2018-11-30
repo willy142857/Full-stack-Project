b0b1f9053799a78889cc045cb2532e673fa24f3a
ChinYa <smallrespect33@gmail.com>
2018-12-01 03:49:12 +0800

    nav上的購物車街上後端

--- src/app/layouts/nav/nav.component.ts
+++ src/app/layouts/nav/nav.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common' ;
 import { ActivatedRoute } from '@angular/router';
+import { CartsService } from 'src/app/cart/carts.service';
+import { ProjectPlus } from 'src/app/project/project';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,16 +12,22 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class NavComponent implements OnInit {
 
-  constructor(private location: Location,
-    private route: ActivatedRoute
+  constructor(
+    private authService: AuthService,
+    private location: Location,
+    private route: ActivatedRoute,
+    private cartsservice: CartsService,
     ) {}
   pathUrl;
   routeUrl;
   current = {
     'current-page-item': false,
   };
+  totalmoney;
+  projectlist: ProjectPlus[];
   ngOnInit() {
     scroll(0, 0);
+    this.checkLogin();
     this.check();
   }
   check() {
@@ -31,5 +40,21 @@ export class NavComponent implements OnInit {
     }
     console.log(this.routeUrl);
     console.log(this.pathUrl);
+  }
+
+  get isLogin() {
+    return this.authService.isLogin();
+  }
+
+  checkLogin() {
+    if (this.authService.isLogin()) {
+      this.cartsservice.getUserAllInfo();
+      this.projectlist = this.cartsservice.followingProjectlist;
+      this.totalmoney = this.cartsservice.totalmoney;
+    }
+  }
+  buttomClickMinus(index) {
+    this.cartsservice.buttomClickMinus(index);
+    this.totalmoney = this.cartsservice.totalmoney;
   }
 }
