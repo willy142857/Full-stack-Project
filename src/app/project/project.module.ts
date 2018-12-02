4c20734c2dcc6c290bbfe0f63ebcbe3554ddb5ce
WillyYang <willy142857@gmail.com>
2018-12-03 04:00:25 +0800

    add project unit

--- src/app/project/project.module.ts
+++ src/app/project/project.module.ts
@@ -6,10 +6,11 @@ import { EditComponent } from './edit/edit.component';
 import { IndexComponent } from './index/index.component';
 import { ProjectRoutingModule } from './project-routing.module';
 import { CommodityComponent } from './commodity/commodity.component';
+import { UnitComponent } from './unit/unit.component';
 
 @NgModule({
   imports: [CommonModule, ProjectRoutingModule, SharedModule],
-  declarations: [IndexComponent, CreateComponent, EditComponent, CommodityComponent],
-  exports: [IndexComponent, CreateComponent, EditComponent, CommodityComponent]
+  declarations: [IndexComponent, CreateComponent, EditComponent, CommodityComponent, UnitComponent],
+  exports: [IndexComponent, CreateComponent, EditComponent, CommodityComponent, UnitComponent]
 })
 export class ProjectModule {}
---
+++ src/app/project/unit/unit.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Project } from '../project';
+import { ProjectService } from '../project.service';
+
+@Component({
+  selector: 'app-project-unit',
+  templateUrl: './unit.component.html',
+  styleUrls: ['./unit.component.css']
+})
+export class UnitComponent implements OnInit {
+  @Input() project: Project;
+
+  constructor(private projectService: ProjectService) { }
+
+  ngOnInit() {
+  }
+
+  progress(project: Project): number {
+    const ratio = (project.curr_amount / project.goal_amount) * 100;
+    return ratio > 100 ? 100 : ratio;
+  }
+  // 計算剩餘日期
+  leftDay(project: Project): number {
+    return this.projectService.calcLeftDay(project);
+  }
+}
