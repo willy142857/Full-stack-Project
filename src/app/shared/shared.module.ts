1a6e8ed972cbf38f0d65c9c13b80e5e3b6bd72e9
WillyYang <willy142857@gmail.com>
2018-12-02 15:16:29 +0800

    增加註冊驗證碼 需安裝npm i ng-recaptcha --save

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
+import { RecaptchaModule, RECAPTCHA_SETTINGS, RecaptchaSettings } from 'ng-recaptcha';
+import { RecaptchaFormsModule } from 'ng-recaptcha/forms';
 
 @NgModule({
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RecaptchaModule, RecaptchaFormsModule],
   declarations: [],
-  exports: [FormsModule, TranslateModule]
+  exports: [FormsModule, TranslateModule, RecaptchaModule, RecaptchaFormsModule],
+  providers: [
+    {
+      provide: RECAPTCHA_SETTINGS,
+      useValue: { siteKey: '6LfDPn4UAAAAABVwigoMYCJcpZr-tjTl6mrpZqZh' } as RecaptchaSettings,
+    },
+  ],
 })
 export class SharedModule {}
