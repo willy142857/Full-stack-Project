9654c582a1f3ab3557ec2b8d3e449a53d391b200
hsu47 <cloud04130413@gmail.com>
2018-12-08 11:42:07 +0800

    加進度顏色

--- src/app/project/unit/unit.component.ts
+++ src/app/project/unit/unit.component.ts
@@ -10,6 +10,7 @@ import { ProjectService } from '../project.service';
 export class UnitComponent implements OnInit {
   @Input() project: Project;
 
+
   constructor(private projectService: ProjectService) { }
 
   ngOnInit() {
@@ -17,10 +18,11 @@ export class UnitComponent implements OnInit {
 
   progress(project: Project): number {
     const ratio = (project.curr_amount / project.goal_amount) * 100;
-    return ratio > 100 ? 100 : ratio;
+    return Math.floor(ratio) > 100 ? 100 : Math.floor(ratio);
   }
   // 計算剩餘日期
   leftDay(project: Project): number {
     return this.projectService.calcLeftDay(project);
   }
 }
+
